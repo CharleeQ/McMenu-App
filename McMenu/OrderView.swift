1c53ea5ce906a73dc54651ec299739bc257230ec
Kirill Kakareko <charleequa@gmail.com>
2021-05-05 21:44:13 +0300

    May 5, '21
    Add close modal screen button

--- McMenu/OrderView.swift
+++ McMenu/OrderView.swift
@@ -9,23 +9,35 @@ import SwiftUI
 
 struct OrderView: View {
     @EnvironmentObject var menu: McMenuViewModel
+    @Environment(\.presentationMode) var presentMode: Binding<PresentationMode>
+    
+    @State private var isPresented = false
     
     var body: some View {
         NavigationView {
-            Form {
-                Section (header: Text("Общее")) {
-                    TwoLabelsRow(title: "Стоимость заказа", value: "\(menu.calculatePrice()) BYN")
-                    TwoLabelsRow(title: "Энергетическая ценность", value: "\(menu.calculateEnergy()) кДж")
-                    TwoLabelsRow(title: "Калорийность", value: "\(menu.calculateCalories()) Кал")
-                }
-                Section (header: Text("Позиции")) {
-                    ForEach (0..<menu.dishes.count) { item in
-                        if menu.dishes[item].countPositions > 0 {
-                            FoodRow(item: $menu.dishes[item])
+            ZStack {
+                Form {
+                    Section (header: Text("Общее")) {
+                        TwoLabelsRow(title: "Стоимость заказа", value: "\(menu.calculatePrice()) BYN")
+                        TwoLabelsRow(title: "Энергетическая ценность", value: "\(menu.calculateEnergy()) кДж")
+                        TwoLabelsRow(title: "Калорийность", value: "\(menu.calculateCalories()) Кал")
+                    }
+                    Section (header: Text("Позиции")) {
+                        ForEach (0..<menu.dishes.count) { item in
+                            if menu.dishes[item].countPositions > 0 {
+                                FoodRow(item: $menu.dishes[item])
+                            }
                         }
+                    }
+                    Button(action: {
+                        presentMode.wrappedValue.dismiss()
+                    }) {
+                        Text("Вернуться в меню")
+                            .foregroundColor(.pink)
                     }
                 }
             }
+            
             .navigationTitle("Заказ")
         }
     }
